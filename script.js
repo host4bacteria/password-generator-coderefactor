3ee490902d70e7b9e8ac06be39a758525d98ce70
host4bacteria <trishtinaldoctor@yahoo.com>
2023-07-19 22:02:56 -0400

    assigned variables

---
+++ script.js
@@ -0,0 +1,19 @@
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialchar = "$%!&@";
+var userChosen = "";
+var password = "";
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
